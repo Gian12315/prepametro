45bab307991d8e1810c6f96cd6b191e3aaa4243a
Gian12315 <giancarlo.cytro@gmail.com>
2025-02-20 22:42:31 -0600

    Yada yada

---
+++ src/posts/first_post.md
@@ -0,0 +1,7 @@
+Title:	Primer Post
+Author: Preparatoria Metropolitano
+Date:	February 19, 2025  
+
+# Bienvenido al sitio de la Preparatoria Metropolitano
+
+Para mas información revisa.
--- src/web.clj
+++ src/web.clj
@@ -2,6 +2,7 @@
   (:require [stasis.core :as stasis]
             [clojure.java.io :as io]
             [hiccup2.core :as h]
+            [optimus.link :as link] ; New
             [optimus.prime :as optimus]
             [optimus.assets :as assets]
             [optimus.optimizations :as optimizations]
@@ -9,34 +10,58 @@
             [optimus.strategies :refer [serve-live-assets]]
             [ring.adapter.jetty :as jt]
             [markdown.core :as md]
+            [clojure.string :as str]
+            [optimus.export]
             [sail.core :as sail]))
 
 ;; Get assets
 
 (defn get-assets []
-  (assets/load-assets "resources/public/" [#"/images/.*\.png"]))
+  (assets/load-assets "public" [#".*"]))
   
 (comment
-  
-  (get-assets))
+ (format "background-image: url('%s');" "hello" 
+  (get-assets)))
+
 
 ;; Define pages
-(defn nav-bar []
+(defn nav-bar [request]
    (h/html
-     [:nav.bg-cover.bg-center.h-1-6 {:style "background-image: url('logo.png');"}
-      [:div.container.mx-auto.h-full..bg-centerflex.flex-col.justify-center.items-center
-       [:h1.text-red.text-3xl.font-bold.mb-4 "Preparatoria Metropolitano"]
-       [:ul.flex.space-x-6
-        [:li
-         [:a.text-red.hover:underline {:href "https://www.google.com"} "Inicio"]]
-        [:li
-         [:a.text-red.hover:underline {:href "https://www.google.com"} "Home"]]
-        [:li
-         [:a.text-red.hover:underline {:href "https://www.google.com"} "Home"]]
-        [:li
-         [:a.text-red.hover:underline {:href "https://www.google.com"} "Home"]]]]]))
+      [:div.relative.font-sans.before:absolute.before:w-full.before:h-full.before:inset-0.before:bg-black.before:opacity-50.before:z-10.h-70
+       [:img
+        {:src "https://readymadeui.com/cardImg.webp",
+         :alt "Banner Image",
+         :class "absolute inset-0 w-full h-full object-cover"}]
+       [:div
+        {:class
+         "relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6"}
+        [:h2
+         {:class "sm:text-4xl text-2xl font-bold mb-6"}
+         "Preparatoria Metropolitano"]
+        [:div.flex.justify-around.w-200.mt-12
 
-(defn layout-page [page]
+         [:a {:href "first_post"}
+          [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
+           {:type "button"}
+           "Book Now"]]
+         [:a
+            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
+             {:type "button"}
+             "Book Now"]]
+         [:a
+            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
+             {:type "button"}
+             "Book Now"]]
+         [:a
+            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
+             {:type "button"}
+             "Book Now"]]
+         [:a
+            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
+             {:type "button"}
+             "Book Now"]]]]]))
+
+(defn layout-page [request page]
   (str
    (h/html
        [:head
@@ -44,14 +69,27 @@
         [:meta {:name "viewport"
                 :content "width=device-width, initial-scale=1.0"}]
         [:title "Preparatoria Metropolitano"]
-        [:link {:rel "stylesheet" :href "./styles/style.css"}]]
+        [:link {:rel "stylesheet" :href (link/file-path request "/styles/style.css")}]
+        [:script {:src "https://unpkg.com/@tailwindcss/browser@4"}]]
        [:body
-        [:div.nav (nav-bar)]
+        [:div.nav (nav-bar request)]
         [:div.body page]])))
 
-(defn about-page [request]
-  (layout-page (slurp (io/resource "partials/about.html"))))
+(defn home-page [request]
+  (->> (h/html
+         (take 10 (repeatedly #(identity [:p.text-slate-500 "HOLA A TODOS BYE BYE"]))))
+    (layout-page request)))
 
+(defn about-page [request]
+  (->> (h/html
+         [:div "HOLA A TODOS"])
+    (layout-page request)))
+
+;; Blogs
+(defn markdown-pages [pages]
+  (zipmap (map #(str/replace % #"\.md$" "/") (keys pages))
+          (map #(fn [req] (layout-page req (md/md-to-html-string %)))
+               (vals pages))))
 (comment 
   (sail/watch "./resources/public/styles/style.css" {:paths ["./src"]})
   (sail/build "./resources/public/styles/style.css" {:paths ["./src"]}))
@@ -59,8 +97,14 @@
 ;; Serve pages
 
 (defn get-pages []
-  (merge (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
-         {"/" about-page}))
+  (merge (stasis/merge-page-sources 
+           {:public (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+            :blog (markdown-pages (stasis/slurp-directory "resources/posts" #".*\.md$"))})
+         {"/" home-page}
+         {"/about/" about-page}))
+
+
+; (def app (stasis/serve-pages get-pages))
     
 (def app (-> (stasis/serve-pages get-pages)
              (optimus/wrap get-assets optimizations/all serve-live-assets)
@@ -71,7 +115,7 @@
   (about-page nil)
   (get-pages)
   (println (handler nil))
-  (println (app {:uri "/"}))
+  (println (app {:uri "/first_post"}))
   (jt/run-jetty app {:port 3000
                      :join? false}))
 
@@ -81,7 +125,10 @@
 ;; Export pages
 (def target-dir "./target")
 (defn export []
-  (stasis/empty-directory! target-dir)
-  (stasis/export-pages (get-pages) target-dir))
+  (let [assets (optimizations/all (get-assets) {})
+          pages (get-pages)]
+      (stasis/empty-directory! target-dir)
+    (optimus.export/save-assets assets target-dir)
+    (stasis/export-pages pages target-dir {:optimus-assets assets})))
 (comment
   (export))

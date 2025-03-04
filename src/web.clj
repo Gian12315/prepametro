3f5e530933e4b7365cd253d20d185f9abce09f5c
Gian12315 <giancarlo.cytro@gmail.com>
2025-03-03 20:34:29 -0600

    yada3

--- src/web.clj
+++ src/web.clj
@@ -18,71 +18,162 @@
 
 (defn get-assets []
   (assets/load-assets "public" [#".*"]))
-  
-(comment
- (format "background-image: url('%s');" "hello" 
-  (get-assets)))
-
 
 ;; Define pages
-(defn nav-bar [request]
+(defn navbar [request]
    (h/html
-      [:div.relative.font-sans.before:absolute.before:w-full.before:h-full.before:inset-0.before:bg-black.before:opacity-50.before:z-10.h-70
-       [:img
-        {:src "https://readymadeui.com/cardImg.webp",
-         :alt "Banner Image",
-         :class "absolute inset-0 w-full h-full object-cover"}]
-       [:div
-        {:class
-         "relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6"}
-        [:h2
-         {:class "sm:text-4xl text-2xl font-bold mb-6"}
-         "Preparatoria Metropolitano"]
-        [:div.flex.justify-around.w-200.mt-12
+      [:section.hero.is-medium
+       {:style {:background (format "center top/cover url('%s');" (link/file-path request "/images/fachada.png"))}}
+       [:div.hero-head
+        [:div.is-flex.is-justify-content-space-around
+         [:div
+          [:a.pl-3 {:href "https://www.facebook.com/profile.php?id=100063620186389"} [:i.fa-brands.fa-facebook]]
+          [:a.pl-3 {:href "mailto:prepametropolitanatam@gmail.com"} [:i.fa-solid.fa-envelope]]]
+         [:div.is-italic.has-text-weight-light
+            [:i.fa-solid.fa-phone.has-text-link]
+            [:span.pl-2.has-text-white "(833) 288 7233"]]]]
+       [:div.hero-body
+        [:div.container.has-text-centered
+         [:h1.title.is-size-1.has-text-white
+          "Preparatoria Metropolitana"]]]
+       [:div.hero-foot
+        [:nav.navbar.is-link
+         {:role "navigation" 
+          :aria-label "main navigation"}
+         [:div.navbar-brand
+          [:a.navbar-burger
+           {:role "button" :aria-label "menu" :aria-expanded "false"}
+           [:span {:aria-hidden "true"}]
+           [:span {:aria-hidden "true"}]
+           [:span {:aria-hidden "true"}]
+           [:span {:aria-hidden "true"}]]]
+         [:div.navbar-menu.is-justify-content-space-evenly.is-size-5
+           [:a.navbar-item.has-text-white {:href "/"} "Inicio"]
+           [:a.navbar-item.has-text-white {:href "/quienessomos.html"} "¿Quienes somos?"]
+           [:a.navbar-item.has-text-white {:href "/niveles.html"} "Niveles"]
+           [:a.navbar-item.has-text-white {:href "/instalaciones.html"} "Instalaciones"]
+           [:a.navbar-item.has-text-white {:href "/ubicacion.html"} "Ubicación"]
+           [:a.navbar-item.has-text-white {:href "/contacto.html"} "Contacto"]
+           [:a.navbar-item.has-text-white {:href "/certificado.html"} "Certificado"]]]]]))
 
-         [:a {:href "first_post"}
-          [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
-           {:type "button"}
-           "Book Now"]]
-         [:a
-            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
-             {:type "button"}
-             "Book Now"]]
-         [:a
-            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
-             {:type "button"}
-             "Book Now"]]
-         [:a
-            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
-             {:type "button"}
-             "Book Now"]]
-         [:a
-            [:button.bg-transparent.text-white.text-base.py-3.px-6.border.border-white.rounded-lg.hover:bg-white.hover:text-black.transition.duration-300
-             {:type "button"}
-             "Book Now"]]]]]))
 
 (defn layout-page [request page]
   (str
    (h/html
+      (h/raw "<!DOCTYPE html>")
+      [:html {:lang "es" :data-theme "light"}
        [:head
         [:meta {:charset "utf-8"}]
         [:meta {:name "viewport"
                 :content "width=device-width, initial-scale=1.0"}]
-        [:title "Preparatoria Metropolitano"]
+        [:title "Preparatoria Metropolitana"]
         [:link {:rel "stylesheet" :href (link/file-path request "/styles/style.css")}]
-        [:script {:src "https://unpkg.com/@tailwindcss/browser@4"}]]
+        [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"}]
+        [:script {:src (link/file-path request "/scripts/dist/bulma.js")}]]
        [:body
-        [:div.nav (nav-bar request)]
-        [:div.body page]])))
+        [:div (navbar request)]
+        [:div.container.px-6.my-6 
+         [:div.box page]]]])))
+
+    
 
 (defn home-page [request]
   (->> (h/html
-         (take 10 (repeatedly #(identity [:p.text-slate-500 "HOLA A TODOS BYE BYE"]))))
+         [:p "Aquí estara un blog, se encuentra en construcción"])
     (layout-page request)))
 
 (defn about-page [request]
   (->> (h/html
+         [:section.section
+          [:div.columns.is-vcentered.is-6
+           [:div.column
+            [:div
+             [:h1.title.is-1 "Misión"]
+             [:p.is-size-5 "Brindar una educación accesible y de calidad a los jóvenes de Tampico, Tamaulipas, fomentando el desarrollo integral de cada estudiante a través de un modelo académico innovador, incluyente y basado en valores. Nos comprometemos a formar ciudadanos críticos, responsables y preparados para enfrentar los retos del futuro, impulsando el crecimiento personal y profesional en un entorno de respeto, equidad y excelencia."]]]
+           [:div.column.is-three-fifths
+            [:img {:src (link/file-path request "/images/estudiantes1.png")}]]]]
+
+         [:section.section
+          [:div.columns.is-vcentered.is-6
+           [:div.column
+            [:div
+             [:h1.title.is-1 "Visión"]
+             [:p.is-size-5 "Ser la preparatoria de referencia en el centro de Tampico, reconocida por su accesibilidad, calidad educativa y compromiso con la comunidad. Aspiramos a formar generaciones de estudiantes con alto nivel académico, conciencia social y habilidades para transformar positivamente su entorno, mediante un modelo educativo dinámico, actualizado y centrado en el bienestar del estudiante."]]]
+           [:div.column.is-three-fifths
+            [:img {:src (link/file-path request "/images/estudiantes2.png")}]]]]
+         [:section.section
+          [:div.columns.is-vcentered.is-6
+           [:div.column
+            [:div.content
+             [:h1.title.is-1 "Valores"]
+             [:dl.is-size-5
+              [:dt.has-text-weight-bold "Excelencia"]
+              [:dd "Buscamos siempre la mejora continua en la enseñanza y el aprendizaje."]
+              [:dt.has-text-weight-bold "Inclusión"]
+              [:dd "Garantizamos un espacio educativo accesible para todos, sin distinción."]
+              [:dt.has-text-weight-bold "Compromiso"]
+              [:dd "Fomentamos la responsabilidad y el esfuerzo como base del éxito."]
+              [:dt.has-text-weight-bold "Respeto"]
+              [:dd "Promovemos la convivencia armoniosa y el reconocimiento de la diversidad."]
+              [:dt.has-text-weight-bold "Innovación"]
+              [:dd "Aplicamos nuevas metodologías y herramientas para un aprendizaje efectivo."]
+              [:dt.has-text-weight-bold "Solidaridad"]
+              [:dd "Impulsamos el apoyo mutuo y el servicio a la comunidad como pilares fundamentales."]]]]
+           [:div.column.is-three-fifths
+            [:img {:src (link/file-path request "/images/estudiantes3.png")}]]]])
+    (layout-page request)))
+
+
+
+
+
+(defn levels-page [request]
+  (->> (h/html
          [:div "HOLA A TODOS"])
+    (layout-page request)))
+
+(defn building-page [request]
+  (->> (h/html
+         [:div "HOLA A TODOS"])
+    (layout-page request)))
+
+(defn location-page [request]
+  (->> (h/html
+         [:div "HOLA A TODOS"])
+    (layout-page request)))
+
+(defn contact-page [request]
+  (->> (h/html
+         [:div.notification.is-success.is-hidden
+          [:button.delete]
+          "Gracias por ponerte en contacto con nosotros, te responderemos tan pronto nos sea posible."]
+         [:h1.title "Contactanos"]
+         [:form.static-form
+           [:div.field
+            [:label.label "Correo"]
+            [:div.control.has-icons-left
+              [:input.input {:type "text" :placeholder "correo@mail.com" :name "email"}]
+              [:span.icon.is-small.is-left
+               [:i.fas.fa-envelope]]]]
+           [:div.field
+            [:label:label "Nombre"]
+            [:div.control
+             [:input.input {:type "text" :placeholder "bla" :name "algo"}]]]])
+
+    (layout-page request)))
+
+(defn certificates-page [request]
+  (->> (h/html
+         [:h1.title "Verifica la validez de tu cerficado"]
+         [:p "En esta pagina puede validar los certificados generados por la preparatoria Metropolitana, para esto, ingresa el folio en el certificado en el siguiente campo de texto:"]
+         [:form.form
+          [:div.field.has-addons.has-addons-centered
+           [:div.control
+             [:input#input.input {:type "text" :placeholder "Folio de tu certificado:"}]]
+           [:div.control
+             [:button#search.button "Buscar"]]]]
+         [:p [:strong "Nota: "] "Si se acaba de generar tu certificado, puede tardar un tiempo máximo de 3 días para ser reflejado en el sistema."]
+         [:script (h/raw (slurp "src/certificate.js"))])
     (layout-page request)))
 
 ;; Blogs
@@ -90,18 +181,20 @@
   (zipmap (map #(str/replace % #"\.md$" "/") (keys pages))
           (map #(fn [req] (layout-page req (md/md-to-html-string %)))
                (vals pages))))
-(comment 
-  (sail/watch "./resources/public/styles/style.css" {:paths ["./src"]})
-  (sail/build "./resources/public/styles/style.css" {:paths ["./src"]}))
 
 ;; Serve pages
 
 (defn get-pages []
   (merge (stasis/merge-page-sources 
-           {:public (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+           {:public (stasis/slurp-directory "resources/public" #".*\.(html|css|js|txt|map)$")
             :blog (markdown-pages (stasis/slurp-directory "resources/posts" #".*\.md$"))})
          {"/" home-page}
-         {"/about/" about-page}))
+         {"/quienessomos.html" about-page}
+         {"/niveles.html" levels-page}
+         {"/instalaciones.html" building-page}
+         {"/ubicacion.html" location-page}
+         {"/contacto.html" contact-page}
+         {"/certificado.html" certificates-page}))
 
 
 ; (def app (stasis/serve-pages get-pages))
